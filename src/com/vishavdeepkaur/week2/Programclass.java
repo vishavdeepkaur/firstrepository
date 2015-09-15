4503ce10ce12bd0857cff7bc1f31a46019fb579c
vishavdeepkaur <vishav.deep27@gmail.com>
2015-09-15 11:27:32 -0400

    added  private name variable

--- src/com/vishavdeepkaur/week2/Programclass.java
+++ src/com/vishavdeepkaur/week2/Programclass.java
@@ -5,10 +5,13 @@ public class Programclass {
 	public Programclass() {
 		// TODO Auto-generated constructor stub
 	}
+	private static String _name;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("program started");
+_name="Vishu";
+System.out.println(_name);
 
 
 	}
