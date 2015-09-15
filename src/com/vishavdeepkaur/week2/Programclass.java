e7883936f5f7d70bed3efe9b2ec90d86a6ab08ef
vishavdeepkaur <vishav.deep27@gmail.com>
2015-09-15 11:13:58 -0400

    sad

---
+++ src/com/vishavdeepkaur/week2/Programclass.java
@@ -0,0 +1,16 @@
+package com.vishavdeepkaur.week2;
+
+public class Programclass {
+
+	public Programclass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("program started");
+
+
+	}
+
+}
---
+++ src/week2/Class2.java
@@ -0,0 +1,14 @@
+package week2;
+
+public class Class2 {
+
+	public Class2() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
